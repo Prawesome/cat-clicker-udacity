da759eb212aa5c9e49e2bb9e53d1de3d5ca068ae
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-18 18:41:02 +0300

    move data to model

--- js/app.js
+++ js/app.js
@@ -1,23 +1,29 @@
-var ViewModel = function() {
+var ViewModel = function () {
+
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCount = function () {
+        this.currentCat().catCount(this.currentCat().catCount() + 1);
+        console.log(this.currentCat().catCount());
+    };
+};
+
+var Cat = function () {
     this.catName = ko.observable('Toby');
     this.catImage = ko.observable('./img/4154543904_6e2428c421_z.jpg');
     this.catCount = ko.observable(0);
 
-    this.incrementCount = function() {
-        this.catCount(this.catCount() + 1);
-    };
-
-    this.catCategory = ko.computed(function() {
-        if(this.catCount() < 10) {
+    this.catCategory = ko.computed(function () {
+        if (this.catCount() < 10) {
             return "child";
-        }  else {
+        } else {
             return "kilavan";
         }
     }, this);
 
     this.catNicks = ko.observableArray([
-        {nick : 'Toby'},
-        {nick : 'Pottan'}
+        { nick: 'Toby' },
+        { nick: 'Pottan' }
     ]);
 };
 
