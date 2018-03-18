5b4d2f5130fc70ef0aa3bde40b044d7dcea79fde
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-18 18:08:29 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var ViewModel = function() {
+    this.catName = ko.observable('Toby');
+    this.catImage = ko.observable('./img/4154543904_6e2428c421_z.jpg');
+    this.catCount = ko.observable(0);
+
+    this.incrementCount = function() {
+        this.catCount(this.catCount() + 1);
+    };
+
+    this.catCategory = ko.computed(function() {
+        if(this.catCount() < 10) {
+            return "child";
+        }  else {
+            return "kilavan";
+        }
+    }, this);
+};
+
+ko.applyBindings(new ViewModel());
