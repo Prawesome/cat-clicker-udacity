7d3d6b0e28d304c0ce43f0dd377753ace2aa6001
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-19 09:39:38 +0300

    final commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,45 @@
-var ViewModel = function () {
+var initCats = [{
+    name: 'Toby',
+    src: './img/4154543904_6e2428c421_z.jpg',
+    count: 0,
+    nick: 'nick1'
+},
+{
+    name: 'Jacki',
+    src: './img/434164568_fea0ad4013_z.jpg',
+    count: 0,
+    nick: 'nick2'
+}, {
+    name: 'kuttu',
+    src: '../img/9648464288_2516b35537_z.jpg',
+    count: 0,
+    nick: 'nick3'
+}];
 
-    this.currentCat = ko.observable(new Cat());
+var ViewModel = function () {
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+    initCats.forEach(cat => {
+        self.catList.push(new Cat(cat));
+    });
+
+    this.currentCat = ko.observable(self.catList()[0]);
 
     this.incrementCount = function () {
         this.catCount(this.catCount() + 1);
     };
+
+    this.changeCat = function(item) {
+        self.currentCat(item);
+    }
 };
 
-var Cat = function () {
-    this.catName = ko.observable('Toby');
-    this.catImage = ko.observable('./img/4154543904_6e2428c421_z.jpg');
-    this.catCount = ko.observable(0);
+var Cat = function (data) {
+    this.catName = ko.observable(data.name);
+    this.catImage = ko.observable(data.src);
+    this.catCount = ko.observable(data.count);
+    this.catNick = ko.observable(data.nick);
 
     this.catCategory = ko.computed(function () {
         if (this.catCount() < 10) {
@@ -19,11 +48,6 @@ var Cat = function () {
             return "kilavan";
         }
     }, this);
-
-    this.catNicks = ko.observableArray([
-        { nick: 'Toby' },
-        { nick: 'Pottan' }
-    ]);
 };
 
 ko.applyBindings(new ViewModel());
